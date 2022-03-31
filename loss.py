600834037f079c8c9b50b103d5d52ea07a1b6ecd
Anh.V Dang <imnova1212@gmail.com>
2022-03-31 18:36:50 +0700

    Update loss.py

--- loss.py
+++ loss.py
@@ -3,14 +3,18 @@ import torch.nn as nn
 import torch.nn.functional as F
 Tensor = torch.Tensor
 
+# hyperparameters for KD
+alphas = [0.99, 0.95, 0.5, 0.1, 0.05]
+temperatures = [20., 10., 8., 6., 4.5, 3., 2., 1.5]
 
-def loss_fn_kd(preds, labels, teacher_preds, T, alpha):
+
+def loss_fn_kd(preds:Tensor, labels:Tensor, teacher_preds:Tensor, T:float, alpha:float):
     return nn.KLDivLoss()(F.log_softmax(preds / T, dim=1),
                           F.softmax(teacher_preds / T, dim=1)) * (alpha * T * T) + F.cross_entropy(preds, labels) * (1. - alpha)
 
 
 
-def distillation_loss(preds:Tensor, labels:Tensor, teacher_preds:Tensor, T, alpha:float):
+def loss_fn_kd(preds:Tensor, labels:Tensor, teacher_preds:Tensor, T:float, alpha:float):
     return F.kl_div(F.log_softmax(preds / T, dim=1), 
                     F.softmax(teacher_preds / T, dim=1),
-                    reduction='batchmean') * T * T * alpha + F.cross_entropy(preds, labels) * (1. - alpha)
+                    reduction='mean', log_target=False) * T * T * alpha + F.cross_entropy(preds, labels) * (1. - alpha)
