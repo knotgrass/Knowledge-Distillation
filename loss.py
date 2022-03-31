ef7f211adc46d040586073b398719e29e90d7933
Anh.V Dang <imnova1212@gmail.com>
2022-03-31 18:37:04 +0700

    Update loss.py

--- loss.py
+++ loss.py
@@ -1,20 +1,16 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-Tensor = torch.Tensor
+import torchvision.models as models
 
-# hyperparameters for KD
-alphas = [0.99, 0.95, 0.5, 0.1, 0.05]
-temperatures = [20., 10., 8., 6., 4.5, 3., 2., 1.5]
+# import timm
 
 
-def loss_fn_kd(preds:Tensor, labels:Tensor, teacher_preds:Tensor, T:float, alpha:float):
-    return nn.KLDivLoss()(F.log_softmax(preds / T, dim=1),
-                          F.softmax(teacher_preds / T, dim=1)) * (alpha * T * T) + F.cross_entropy(preds, labels) * (1. - alpha)
+teacher = models.resnet152(pretrained= False, progress= True)
+teacher.fc = nn.Linear(in_features=teacher.fc.in_features,
+                       out_features=100, bias= True)
 
 
+student = models.resnet18(pretrained= False, progress= True)
+student.fc = nn.Linear(in_features=student.fc.in_features,
+                       out_features=100, bias= True)
 
-def loss_fn_kd(preds:Tensor, labels:Tensor, teacher_preds:Tensor, T:float, alpha:float):
-    return F.kl_div(F.log_softmax(preds / T, dim=1), 
-                    F.softmax(teacher_preds / T, dim=1),
-                    reduction='mean', log_target=False) * T * T * alpha + F.cross_entropy(preds, labels) * (1. - alpha)
