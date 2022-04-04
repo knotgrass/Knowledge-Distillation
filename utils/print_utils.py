d65d16b5eb636df6a85346055392b6e5b9c51329
Anh.V Dang <imnova1212@gmail.com>
2022-04-04 17:39:07 +0700

    add print_time

--- utils/print_utils.py
+++ utils/print_utils.py
@@ -1,3 +1,4 @@
+import math
 from colorama import Fore
 # https://www.tecmint.com/boxes-draws-ascii-art-boxes-in-linux-terminal/
 # https://www.w3resource.com/python/python-format.php
@@ -8,7 +9,7 @@ from colorama import Fore
 # └──────────────────────(v1.0)┘
 
 
-def print_msg(title:str, msg:str, *args):
+def print_msg(title:str, msg:str, *args) -> None:
     title = Fore.RED +title.strip()+' ' + Fore.RESET
     print('┌─ {:─<70}┐'.format(title))
     print('│', '{:>62}'.format('│'))
@@ -16,6 +17,14 @@ def print_msg(title:str, msg:str, *args):
     print('│', '{:>62}'.format('│'))
     print('└{:─>61}─┘'.format('(v1.0)'))
 
-def print_time(): ...
-#TODO
-# print_msg("Unfreeze all layers","Resnet")
+def print_time(title:str, time:float):
+    minute = int(time // 60)
+    milisec, sec = math.modf(time % 60)
+    print('{}  {} :{:^3d}:{:>3d}'.format(
+        title, minute, int(sec), int(1000*milisec)))
+    
+
+def print_metric(metric_name:str, result:float) -> None:
+    ...
+time = 12 * 60 +  1.007
+print_time("CLASSIFIER TRAINING TIME", time)
