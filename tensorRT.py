78a69a2fc9800ae7debd782799e07549cd80dc7e
watson21 <imnova1212@gmail.com>
2022-05-16 22:47:42 +0700

    convert model to tensorrt

---
+++ tensorRT.py
@@ -0,0 +1,24 @@
+import torch
+from torch2trt import torch2trt
+from torchvision.models.alexnet import alexnet
+from time import time
+# create some regular pytorch model...
+model = alexnet(pretrained=False).eval().cuda()
+
+# create example data
+x = torch.ones((1, 3, 224, 224)).cuda()
+torch.Tensor
+# convert to TensorRT feeding sample data as input
+model_trt = torch2trt(model, [x])
+
+t = time()
+y = model(x)#.detach()
+tt = time()
+print(y.argmax(), tt-t)
+
+s = time()
+y_trt = model_trt(x)#.detach()
+e = time()
+print(y_trt.argmax(), e-s)
+
+
