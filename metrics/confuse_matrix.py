56a9710322bc83e29cf6d8868356c15f9e71ac64
Watson21 <imnova1212@gmail.com>
2022-05-20 17:20:37 +0700

    create pretty_confusion_matrix

--- metrics/confuse_matrix.py
+++ metrics/confuse_matrix.py
@@ -1,16 +1,58 @@
+import matplotlib.pyplot as plt
 import numpy as np
-import math
-from ..dataloader import num_classes
+import pandas as pd
+import seaborn as sns
+from pretty_confusion_matrix import pp_matrix, pp_matrix_from_data
+
+# np.set_printoptions(precision=5, suppress= True)
 
 
-def create_matrix(dataset_size:int, acc:float, precision:float, recall:float) -> np.ndarray:
-    # use acc for diagonal of matrix
-    # các class sai sẽ tuân theo U(0, 1)
-    # precision và recall để tính số lượng class sai này
-    # qua mỗi epoch sẽ tạo lại matrix 1 lần theo hướng tăng cấc chỉ số lên
-    # tất cả đều random lại
-    tp = math.floor(dataset_size * acc) # true positive
-    fn = dataset_size - tp
+path_csv = r'C:\Users\adang1\Documents\SuntoryImageDB-dev_python\temp\evaluation.csv'
+
+# test = r'C:\Users\adang1\Documents\SuntoryImageDB-dev_python\train_datasets\cls\test_classify'
+
+table = pd.read_csv(path_csv)
+# print(table.shape)
+gt = table['gt'].tolist()
+pred = table['pred'].tolist()
+size = len(set(gt))
+
+classes = [
+        "TP_1",
+        "TP_2",
+        "TP_3",
+        "TP_4",
+        "TP_5",
+        "TP_6",
+        "TP_7",
+        "TP_8",
+        "TP_unknown",
+        
+        "FN_1",
+        "FN_2",
+        "FN_3",
+        "FN_4",
+        "FN_5",
+        "FN_6",
+        "FN_7",
+        "FN_unknown"]
+
+# print(len(classes))
+# print(set(gt))
+# print(set(pred))
+
+# def matrix(gt, pred):
+#     assert len(gt) == len(pred), 'len(gt) != len(pred)'
+#     confuse_m = np.zeros((size, size), dtype= np.int16)
     
-    # tính số lượng class sai dự vào fn
-    
+#     for g, p in zip(gt, pred):
+#         confuse_m [p][g] += 1
+
+#     return confuse_m
+
+# confuse_m = matrix(gt, pred)
+# df_cm = pd.DataFrame(confuse_m, index=range(1, size+1), columns=range(1, size+1))
+# pp_matrix(df_cm, cmap='YlGnBu')
+
+# sns.dark_palette("#69d", reverse=True, as_cmap=True)
+pp_matrix_from_data(y_test=gt, predictions=pred, columns=classes, cmap="RdBu")
