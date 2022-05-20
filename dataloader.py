9b6c0fe38853c5a705df7808648cc79e65f2a3c4
Watson21 <imnova1212@gmail.com>
2022-05-20 15:39:33 +0700

    add albumentation dataset
    write custom dataset using albumentations

--- dataloader.py
+++ dataloader.py
@@ -1,48 +1,127 @@
 import os
+from typing import Any, Callable, Optional, Tuple
+
+import albumentations as A
+import cv2
+import numpy as np
 from colorama import Fore
+from torch.utils.data import ConcatDataset, DataLoader, random_split
+# from torchvision.datasets import CIFAR100, ImageFolder, DatasetFolder
+from torchvision.datasets.folder import (IMG_EXTENSIONS, DatasetFolder,
+                                         ImageFolder)
 
-from torch.utils.data import DataLoader, random_split
-from torchvision.datasets import CIFAR100#, ImageFolder
+from config.cfg import batch_size, dataset_root, generator, transformers
 
-from config import cfg
-
-
-cifar100 = CIFAR100('dataset', train=True, download=True,
-                    transform=cfg.transformers['original'])
-
-dataset_size = len(cifar100)
-
-test_size = round(0.2*dataset_size)
-val_size = round(0.1*dataset_size)
-train_size = dataset_size - val_size - test_size
-
-generator = cfg.generator
-
-train, val, test = random_split(cifar100, [train_size, val_size, test_size],
-                                generator=generator)
-
-batch_size = cfg.batch_size
 n_workers = os.cpu_count()
-loaders = {}
-loaders['train'] = DataLoader(train, batch_size=batch_size, shuffle=True,
-                              num_workers=n_workers, generator=generator, pin_memory=True)
-loaders['val'] = DataLoader(val, batch_size=batch_size, shuffle=True,
-                            num_workers=n_workers, generator=generator, pin_memory=True)
-loaders['test'] = DataLoader(test, batch_size=batch_size, shuffle=True,
-                             num_workers=n_workers, generator=generator, pin_memory=True)
-del generator
+
+class AlbumDatasetFolder(DatasetFolder):
+    def __getitem__(self, index: int) -> Tuple[Any, Any]:
+        """
+        Args:
+            index (int): Index
+
+        Returns:
+            tuple: (sample, target) where target is class_index of the target class.
+        """
+        path, target = self.samples[index]
+        sample = self.loader(path)
+        if self.transform is not None:
+            sample = self.transform(image=sample)['image']
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return sample, target
+    
+
+def albumen_loader(path:str) -> np.ndarray:
+    # fpath = os.path.join(image_dir_path, fn)
+    # img = cv2.imdecode(np.fromfile(fpath), cv2.IMREAD_COLOR)
+    # img = cv2.imread(fpath)
+    return cv2.cvtColor(cv2.imread(path), cv2.COLOR_BGR2RGB)
+
+class AlbumImageFolder(AlbumDatasetFolder):
+    
+    def __init__(
+            self,
+            root: str,
+            transform: Optional[Callable] = None,
+            target_transform: Optional[Callable] = None,
+            loader: Callable[[str], Any] = albumen_loader,
+            is_valid_file: Optional[Callable[[str], bool]] = None,
+    ):
+        super(AlbumImageFolder, self).__init__(root, loader, IMG_EXTENSIONS if is_valid_file is None else None,
+                                          transform=transform,
+                                          target_transform=target_transform,
+                                          is_valid_file=is_valid_file)
+        self.imgs = self.samples
+
+
+# class AlbumImageFolder(ImageFolder):
+#     def __init__(
+#             self,
+#             root: str,
+#             transform: Optional[Callable] = None,
+#             target_transform: Optional[Callable] = None,
+#             loader: Callable[[str], Any] = albumen_loader,
+#             is_valid_file: Optional[Callable[[str], bool]] = None,
+#     ):
+#         super(AlbumImageFolder, self).__init__(root, loader, IMG_EXTENSIONS if is_valid_file is None else None,
+#                                           transform=transform,
+#                                           target_transform=target_transform,
+#                                           is_valid_file=is_valid_file)
+#         self.imgs = self.samples
+
+#     def __getitem__(self, index: int) -> Tuple[Any, Any]:
+#         """
+#         Args:
+#             index (int): Index
+
+#         Returns:
+#             tuple: (sample, target) where target is class_index of the target class.
+#         """
+#         path, target = self.samples[index]
+#         sample = self.loader(path)
+#         if self.transform is not None:
+#             sample = self.transform(image=sample)['image']
+#         if self.target_transform is not None:
+#             target = self.target_transform(target)
+
+#         return sample, target
+
+
+origin = ImageFolder(root=dataset_root, transform=transformers['original'])
+light = AlbumImageFolder(root=dataset_root, transform=transformers['light'])
+medium = AlbumImageFolder(root=dataset_root, transform=transformers['medium'])
+strong = AlbumImageFolder(root=dataset_root, transform=transformers['strong'])
+
+
+test_size = round(0.2*len(origin))
+train_val, test = random_split(origin, [len(origin)-test_size, test_size], generator=generator)
+
+train_val = ConcatDataset([train_val, light, medium, strong])
+num_classes = len(origin.class_to_idx)
+class_to_idx = origin.class_to_idx
+classes = origin.classes
+
+del light, medium, strong, origin
+val_size = round(0.1*len(train_val))
+train_size = len(train_val) - val_size
+
+train, val = random_split(train_val, [train_size, val_size], generator=generator)
+
+loaders = {
+'train':DataLoader(train, batch_size, shuffle=True, num_workers=n_workers, pin_memory=True, generator=generator),
+'val':DataLoader(val, batch_size, shuffle=True, num_workers=n_workers, pin_memory=True, generator=generator),
+'test':DataLoader(test, batch_size, shuffle=True, num_workers=n_workers, pin_memory=True, generator=generator)
+}
 
 dataset_sizes = {
-    'train': train.__len__(),
-    'val': val.__len__(),
-    'test': test.__len__(),
+    'train': train_size,
+    'val': val_size,
+    'test': test_size,
 }
-num_classes = len(cifar100.class_to_idx)
 
-# for classname, index in cifar100.class_to_idx.items():
-#     print(index, Fore.RED, classname, Fore.RESET)
-class_to_idx = cifar100.class_to_idx
-idx_to_class = {y: x for x, y in cifar100.class_to_idx.items()}
-# for k, v in index_to_class.items():
-#     print(k, v)
+if __name__ == '__main__':
+    for idx, cls in enumerate(classes):
+        print(Fore.RED,idx,Fore.RESET, cls)
 
