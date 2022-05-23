e41741451f3797940498e04e9e354e53e2231ca4
watson21 <imnova1212@gmail.com>
2022-05-23 16:14:07 +0700

    add albumen_transform

--- inference.py
+++ inference.py
@@ -1,20 +1,24 @@
-import torch
+import torch, timm
 import torch.nn as nn
 import torchvision.models as models
 import torchvision.transforms as T
-
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
 from colorama import Fore
 import numpy as np
 import cv2
 # from scipy.special import softmax
 from distiller.loss import softmax
 
-from dataloader import loaders
-from models.model import teacher
+# from prepare_dataloader import loaders
+
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 class Student(object):
-    def __init__(self, input_size:tuple, mean, std, num_classes) -> None:
+    def __init__(self, input_size:tuple, num_classes:int, 
+                 mean:tuple=(0.485, 0.456, 0.406), 
+                 std:tuple=(0.229, 0.224, 0.225)) -> None:
+        
         # create model
         self.model = models.resnet18(pretrained=False, progress=True)
         self.model.fc = nn.Linear(in_features=self.model.fc.in_features,
@@ -31,15 +35,21 @@ class Student(object):
         self.model.to(self.device).eval()
         
         # tranform datas
-        self.tranform = T.Compose([
+        self.transform = T.Compose([
             T.Resize(input_size),
             T.ToTensor(),
             T.Normalize(mean, std)
     ])
+        
+        self.albumen_transform = A.Compose([
+            A.Resize(*input_size, always_apply=True),
+            A.Normalize(mean=mean, std=std, always_apply=True),
+            ToTensorV2(always_apply=True),
+        ], p=1.0)
 
     @torch.no_grad
     def inference(self, img:np.ndarray) -> tuple:
-        img = self.tranform(img)
+        img = self.transform(img)
         out = self.model(img.to(self.device)).detach().cpu().numpy()
         idx = out.argmax()
         dist = softmax(out)
