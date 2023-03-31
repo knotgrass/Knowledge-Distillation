8eb24cca81478ba4747f7ad3ba240e23839328b6
vietanh112 <imnova1212@gmail.com>
2023-03-31 16:56:44 +0700

    torch.distributions.Normal

--- distiller/pseudo_label.py
+++ distiller/pseudo_label.py
@@ -1,6 +1,3 @@
-import torch
-
-
 """
 1. init one-hot encoding 
 generate normal distribution, sắp xếp lại, lấy giá trị lớn nhất cho y_label
@@ -16,5 +13,21 @@ vẫn đảm bảo vector `y` tuân theo normal distribution
 3. Tại epoch cuối cùng
 `torch.Tensor[y_label] -> 1` tiến đến 1
 `các phần từ còn lại -> 0 ` tiến đến 0
-"""
 
+4. Train với nhãn có phần tử nhỏ hơn 0. Ví dụ: [0.1, -0.3, 0.2, ]
+"""
+"https://pytorch.org/docs/stable/distributions.html#normal"
+
+
+import torch
+from torch.distributions import Normal
+
+num_class = 10
+mu = 1. / num_class
+
+sigma = 1. # std
+m = Normal(loc=mu, scale=sigma)
+y = m.sample((10,))
+
+print(y.shape)
+print(y)
